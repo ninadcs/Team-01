84521bb4996c15444e2e39d767fe9d1014c41c98
Ninad Patankarc <220327677@aston.ac.uk>
2022-12-03 11:19:33 +0000

    Min and Max function addedd

--- Math/src/MathFunction.java
+++ Math/src/MathFunction.java
@@ -1,2 +1,19 @@
+
+
+
+
+
 public class MathFunction {
+    public static void main(String args[])
+    {
+        int a1 = 13;
+        int a2 = 50;
+
+        System.out.println(Math.min(a1,a2));
+        System.out.println(Math.max(a1,a2));
+
+
+    }
 }
+
+
