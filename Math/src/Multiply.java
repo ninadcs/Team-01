fea7ffd9e9a4dfda78f0885bc5a75274643aed85
richard josiah <r.jn@live.com>
2022-12-05 13:21:02 +0000

    multiplication done

---
+++ Math/src/Multiply.java
@@ -0,0 +1,12 @@
+public class Multiply {
+    public static void main(String[] args) {
+
+        int a = 16;
+        int b = 2;
+
+        int result = a * b;
+        System.out.println(result);
+
+    }
+}
+
