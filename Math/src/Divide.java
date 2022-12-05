bb7610259ca92da6512ec1fd87cbcbe93d188db8
Emandeep1511 <220339832@aston.ac.uk>
2022-12-05 13:07:00 +0000

    Divide Function addedd

---
+++ Math/src/Divide.java
@@ -0,0 +1,10 @@
+public class Divide {
+
+    public static void main(String args[]){
+        int a1 = 18;
+        int a2 = 2;
+        int result = a1/a2;
+        System.out.println(result);
+
+    }
+}
